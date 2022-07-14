a3b1d1b063aa2cf7b87d8a027aec8afd7eed628d
Jose Luis <joseluis.elizondof11@outlook.com>
2022-07-14 13:24:49 -0500

    First commit for backup

---
+++ React course/excercise_of_thought.js
@@ -0,0 +1,12 @@
+const page=(
+    <div>
+        <h1>The coolest page you'll probably watch</h1>
+        <p>Idk</p>
+        <button>Click me!</button>
+        <p>Don't click it, it doesn't do shit</p>
+    </div>
+)
+ReactDOM.render(
+    page,
+    root
+)
---
+++ React course/script.js
@@ -0,0 +1,29 @@
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+console.log(page)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
+
+const navbar=(
+    <nav>
+        <h1>Coolest website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    root
+)
